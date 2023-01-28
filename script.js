b194c8bec31ebabdd4f1c7b46663b1d653cbb03a
Zufarbek Zaynutdinov <63949828+zufarbekz@users.noreply.github.com>
2023-01-28 19:58:05 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const startImg = document.getElementById("ecg-image");
+const startBtn = document.getElementById("start-training");
+
+// const training = document.getElementById("training");
+
+// startBtn.addEventListener("click", function () {
+
+//     var count = 4;
+//     var intervalId = setInterval(function() {
+//         document.getElementById("timer").innerHTML = count;
+//         count--;
+//         if (count === -1) {
+//             clearInterval(intervalId);
+//         }
+//     }, 1000);
+
+
+//     training.style.display = "block";
+//     startBtn.style.display = "none";
+
+    
+
+//     setTimeout(function(){
+//         training.style.display = "none";
+//     }, 5000);   
+
+    
+    
+//     setTimeout(function(){
+//         startBtn.style.display = "block";
+//     }, 5001);
+    
+// })
+
+// --------------------------------------
