842623590847b8a9338ed08d1b53b7cd217d4714
Zufarbek Zaynutdinov <63949828+zufarbekz@users.noreply.github.com>
2023-02-16 00:57:24 +0500

    azure add record added

--- azure.js
+++ azure.js
@@ -1,23 +1,49 @@
 var Connection = require('tedious').Connection;
-var config = {
-  server: 'eyetechserver.database.windows.net',
-  authentication: {
-    type: 'default',
+var Request = require('tedious').Request  
+var TYPES = require('tedious').TYPES;  
+
+var config = {  
+    server: 'eyetechserver.database.windows.net', 
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'eyetechuser', 
+            password: 'Umsl@8932' 
+        }
+    },
     options: {
-      userName: 'eyetechuser',
-      password: 'Umsl@8932'
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'EyeTechDatabase'  
     }
-  },
-  options: {
-    encrypt: true,
-    database: 'EyeTechDatabase'
-  }
-};
-var connection = new Connection(config);
+};  
+var connection = new Connection(config);  
 connection.on('connect', function(err) {
-  // If no error, then good to proceed.
-  console.log("Connected");
+    addNewRecord('test2');
+    console.log("Connected");  
 });
 
 connection.connect();
 
+function addNewRecord(videoData) {
+    var request = new Request("INSERT eye_tech_records (video_data) OUTPUT INSERTED.id VALUES (@video_data);", function(err) {  
+     if (err) {  
+        console.log(err);}  
+    });  
+    request.addParameter('video_data', TYPES.NVarChar,videoData); 
+    request.on('row', function(columns) {  
+        columns.forEach(function(column) {  
+          if (column.value === null) {  
+            console.log('NULL');  
+          } else {  
+            console.log("Inserted " + column.value);  
+          }  
+        });
+    });
+
+    // Close the connection after the final event emitted by the request, after the callback passes
+    request.on("requestCompleted", function (rowCount, more) {
+        connection.close();
+    });
+    connection.execSql(request);  
+}  
